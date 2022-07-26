db80e19fb907566b85878960dc1dd899b07b22ec
Fitra Fatimah Putri <fitrafputri11@gmail.com>
2022-07-26 11:50:21 +0700

    update pertanggal 25 juli

--- src/app/page/thread-detail/thread-detail.module.ts
+++ src/app/page/thread-detail/thread-detail.module.ts
@@ -3,12 +3,23 @@ import { ThreadDetailComponent } from "./thread-detail.component";
 import { ThreadDetailRouting } from "./thread-detail.routing";
 import {ButtonModule} from 'primeng/button';
 import {BreadcrumbModule} from 'primeng/breadcrumb';
-
+import {CardModule} from 'primeng/card';
+import { FormsModule } from "@angular/forms";
+import { CommonModule } from "@angular/common";
+import {InputTextModule} from 'primeng/inputtext';
+import {InputTextareaModule} from 'primeng/inputtextarea';
+import {DividerModule} from 'primeng/divider';
 @NgModule({
     imports:[
         ThreadDetailRouting,
         ButtonModule,
-        BreadcrumbModule
+        BreadcrumbModule,
+        CardModule,
+        FormsModule,
+        CommonModule,
+        InputTextModule,
+        InputTextareaModule,
+        DividerModule
     ],
     declarations:[
         ThreadDetailComponent
