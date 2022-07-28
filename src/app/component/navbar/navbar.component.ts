543dad84c28a04538728340cb04f7a7699e425d5
Aldi Mulia Wijaya <deadreaperco@gmail.com>
2022-07-28 15:00:39 +0700

    admin dashboard done

--- src/app/component/navbar/navbar.component.ts
+++ src/app/component/navbar/navbar.component.ts
@@ -1,32 +1,52 @@
 import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
 import {MenuItem} from 'primeng/api';
+
 @Component({
-    selector: 'app-navbar',
-    templateUrl: './navbar.component.html'
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html'
 
 })
-export class NavbarComponent implements OnInit{
-    items : MenuItem[]=[];
-    constructor( public router: Router) { }
-    ngOnInit() {
-        this.items = [
-            {
-                label: 'Menu',
-                items: [
-                    {label: 'Landing', routerLink:'/landing'},
-                    {label: 'Login', routerLink:'/login'},
-                    {label: 'Register', routerLink:'/register'},
-                    {label: 'Home', routerLink:'/home-member'},
-                    {label: 'Thread-Detail', routerLink:'/thread-detail'},
-                    {label: 'Event', routerLink:'/event'},
-                ]
-            }
-        ];
-    }
-    logout(): void {
-        localStorage.clear()
-        this.router.navigateByUrl('/login')
-    }
+export class NavbarComponent implements OnInit {
+  items: MenuItem[] = [];
+  constructor(public router: Router) {}
+  ngOnInit() {
+    this.items = [{
+      label: 'Menu',
+      items: [{
+          label: 'Landing',
+          routerLink: '/landing'
+        },
+        {
+          label: 'Login',
+          routerLink: '/login'
+        },
+        {
+          label: 'Register',
+          routerLink: '/register'
+        },
+        {
+          label: 'Home',
+          routerLink: '/home-member'
+        },
+        {
+          label: 'Thread-Detail',
+          routerLink: '/thread-detail'
+        },
+        {
+          label: 'Event',
+          routerLink: '/event'
+        },
+      ]
+    },
+    {
+      label: 'To Admin',
+      routerLink: '/admin'
+    }];
+  }
+  logout(): void {
+    localStorage.clear()
+    this.router.navigateByUrl('/login')
+  }
 
 }
