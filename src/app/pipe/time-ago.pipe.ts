7eb982db63944352b53f13cde3b418efd70528d9
Muhammad Reza Firmansyah <fmuhammadreza3@gmail.com>
2022-08-04 17:12:44 +0700

    backup reza

---
+++ src/app/pipe/time-ago.pipe.ts
@@ -0,0 +1,34 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+    name: 'timeAgo'
+})
+export class TimeAgoPipe implements PipeTransform {
+    transform(value: any): any {
+        if (value) {
+            const seconds = Math.floor((+new Date() - +new Date(value)) / 1000);
+            if (seconds < 29)
+                return 'just now';
+            const intervals: any = {
+                'year': 31536000,
+                'month': 2592000,
+                'week': 604800,
+                'day': 86400,
+                'hour': 3600,
+                'minute': 60,
+                'second': 1
+            };
+            let counter;
+            for (const i in intervals) {
+                counter = Math.floor(seconds / intervals[i]);
+                if (counter > 0)
+                    if (counter === 1) {
+                        return counter + ' ' + i + ' ago'; // singular
+                    } else {
+                        return counter + ' ' + i + 's ago'; // plural
+                    }
+            }
+        }
+        return value;
+    }
+}
