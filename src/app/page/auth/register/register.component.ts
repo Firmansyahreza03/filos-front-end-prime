293ade7f6789adce856eb61bae5f62c288e27779
Aldi Mulia Wijaya <deadreaperco@gmail.com>
2022-08-05 16:05:44 +0700

    backup aldi

--- src/app/page/auth/register/register.component.ts
+++ src/app/page/auth/register/register.component.ts
@@ -91,10 +91,6 @@ export class RegisterComponent implements OnInit, OnDestroy {
         }
     }
 
-    login():void{
-        this.router.navigateByUrl('/login')
-    }
-
     onSubmitProfilePic(): void {
         this.registerSubscription = this.registerService.register(this.register).subscribe((res) => {
             this.router.navigateByUrl("/login");
