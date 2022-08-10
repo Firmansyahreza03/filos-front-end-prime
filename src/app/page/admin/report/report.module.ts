8d1542937e06949f089b336fef65dbae5890a2c1
Fitra Fatimah Putri <fitrafputri11@gmail.com>
2022-08-10 17:33:07 +0700

    updated 10 august

--- src/app/page/admin/report/report.module.ts
+++ src/app/page/admin/report/report.module.ts
@@ -3,19 +3,14 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { HttpClientModule } from "@angular/common/http";
 import { FormsModule } from "@angular/forms";
-
 import { CardModule } from 'primeng/card';
-import { TableModule } from 'primeng/table';
-import { RippleModule } from 'primeng/ripple';
 import { ButtonModule } from 'primeng/button';
 import { DividerModule } from 'primeng/divider';
 import { InputTextModule } from 'primeng/inputtext';
-import { InputSwitchModule } from 'primeng/inputswitch';
 import { ConfirmDialogModule } from 'primeng/confirmdialog';
 import { InputTextareaModule } from 'primeng/inputtextarea';
 import { CalendarModule } from 'primeng/calendar';
 import { ReportRouting } from './report.routing';
-import { DialogModule } from 'primeng/dialog';
 import { ScrollPanelModule } from 'primeng/scrollpanel';
 import { DropdownModule } from 'primeng/dropdown';
 
